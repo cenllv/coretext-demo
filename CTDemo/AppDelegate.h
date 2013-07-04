c9509ae4fc194d0ea664b640b3d38b8de95fa86b
Martins Rudens <martins.rudens@gmail.com>
2013-07-04 13:11:42 +0300

    Initial Commit

---
+++ CTDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CTDemo
+//
+//  Created by Martins Rudens on 7/4/13.
+//  Copyright (c) 2013 IDEA BITS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
